9d49c48b086d598a8095f6df8f7dc6a44ef478f2
William05Cardozo <3784@holbertonschool.com>
2022-05-08 21:24:10 -0700

    0x1B, DeBug #4, README, Task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# DeBug #4
+exec { 'this will output stuff':
+  path      => '/bin',
+  command   => 'echo "ULIMIT=\"-n 25000\"" > /etc/default/nginx',
+  logoutput => true,
+}
+
+exec { 'nging restart':
+    path    => '/etc/init.d',
+    command => 'nginx restart'
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,2 @@
+Project 0x1B: DeBug #4
+Start: 08/05/2.022 - 11:13pm
