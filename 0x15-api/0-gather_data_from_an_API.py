8a42e4de0e0cf644c6d84b1f7e6eb4bd88d87409
William05Cardozo <3784@holbertonschool.com>
2022-05-19 23:37:50 -0700

    Task 0, Gather_Data, corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,8 +5,8 @@ from sys import argv
 i = 0
 j = 0
 if __name__ == "__main__":
-    url = requests.get('https://jsonplaceholder.typicode.com/users/' + 
-                     argv[1])
+    url = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                       argv[1])
     url_json = url.json()
     task = ""
     todo = requests.get('https://jsonplaceholder.typicode.com/todo')
@@ -16,6 +16,6 @@ if __name__ == "__main__":
                 task += "\t " + t['title'] + '\n'
                 i += 1
             j += 1
-    print("Employee {} is done with task({}\{}):".format(
+    print("Employee {} is done with task({}/{}):".format(
            url_json['name'], i, j))
     print(task, end='')
