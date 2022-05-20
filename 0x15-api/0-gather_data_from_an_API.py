787f1c1efe8c776039cba8baa77fd1531c0769fd
William05Cardozo <3784@holbertonschool.com>
2022-05-19 23:20:12 -0700

    0x15-API, Task 0, Gather_Data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This script using a REST API"""
+import requests
+from sys import argv
+i = 0
+j = 0
+if __name__ == "__main__":
+    url = requests.get('https://jsonplaceholder.typicode.com/users/' + 
+                     argv[1])
+    url_json = url.json()
+    task = ""
+    todo = requests.get('https://jsonplaceholder.typicode.com/todo')
+    for t in todo.json():
+        if t.get('userId') == int(argvv[1]):
+            if t['completed']:
+                task += "\t " + t['title'] + '\n'
+                i += 1
+            j += 1
+    print("Employee {} is done with task({}\{}):".format(
+           url_json['name'], i, j))
+    print(task, end='')
---
+++ 0x15-api/README.md
@@ -0,0 +1,2 @@
+Project 0x15: API
+Start: 19/05/2.022 - 11:24pm
