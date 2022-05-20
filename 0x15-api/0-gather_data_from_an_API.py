cc598ecd65e940cc01f59f0b2706739979c21b05
William05Cardozo <3784@holbertonschool.com>
2022-05-19 23:57:55 -0700

    Task 0, Gather_Data, corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,20 +2,21 @@
 """This script using a REST API"""
 import requests
 from sys import argv
+
 i = 0
 j = 0
 if __name__ == "__main__":
-    url = requests.get('https://jsonplaceholder.typicode.com/users/' +
-                       argv[1])
-    url_json = url.json()
+    user = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                        argv[1])
+    user_json = user.json()
     task = ""
-    todo = requests.get('https://jsonplaceholder.typicode.com/todo')
-    for t in todo.json():
-        if t.get('userId') == int(argvv[1]):
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    for t in todos.json():
+        if t.get('userId') == int(argv[1]):
             if t['completed']:
                 task += "\t " + t['title'] + '\n'
                 i += 1
             j += 1
-    print("Employee {} is done with task({}/{}):".format(
-           url_json['name'], i, j))
+    print("Employee {} is done with tasks({}/{}):".format(
+           user_json['name'], i, j))
     print(task, end='')
