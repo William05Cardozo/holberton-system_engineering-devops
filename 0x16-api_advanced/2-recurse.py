3160ec3932e44dc527ca9255e5c1d0b21ac16dd4
William05Cardozo <3784@holbertonschool.com>
2022-05-22 14:38:16 -0700

    Task 2, Recurse_Corrections

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-"""
-This script return a list containing the titles
-Using a RecursiveFunction
-"""
 
 import requests
 
 
-def recurse(subreddit, hot_list=[],):
-
-    global after
-    headers = {'User-Agent': 'my-app/0.0.1'}
-    try:
-        url = "https://www.reddit.com/r/{}.json".format(sudreddit)
-        if after:
-            url = "https://www.reddit.com/r/{}.json?after={}".format(after)
-        gett = get(url, headers=headers, allow_redirects=False).json()
-        for children in gett['data']['children']:
-            hot_list.append(children.get('data').get('title'))
-        after = get(url, headers=headers,
-                    allow_redirects=False).json().get('data').get('after')
-        if ater:
-            return(recurse(sudreddit, hot_list))
+def recurse(subreddit, hot_list=[], after=""):
+    if (after is None):
         return(hot_list)
-    except Exception:
-        return("None")
+
+    if (len(hot_list) == 0):
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    else:
+        url = "https://www.reddit.com/r/{}/hot.json?after={}".format(
+            subreddit, after)
+    headers = {'user-agent': 'philsrequest'}
+
+    rq = requests.get(url, headers=headers)
+    if (rq.status_code == 404):
+        return(None)
+    elif 'data' not in rq.json():
+        return(None)
+    else:
+        rq = rq.json()
+        for post in rq['data']['children']:
+            hot_list.append(post['data']['title'])
+
+    after = rq['data']['after']
+    return(recurse(subreddit, hot_list, after))
